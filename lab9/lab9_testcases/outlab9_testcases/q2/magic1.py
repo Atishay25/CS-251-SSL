a2cd794eac6c1c185bf081a604e564cec9c5ffea
Atishay25 <atishayjain2552@gmail.com>
2022-11-15 18:52:34 +0530

    Removing extra files in lab9

--- lab9/lab9_testcases/outlab9_testcases/q2/magic1.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def getMagicNumber(x):
-    num = x - 5
-    print(num)
-    return num
--- lab9/q2/magic.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def getMagicNumber(x):
-    num = random.randint(x,x+20)
-    print(num)
-    return num
